d100ddf209d98a3e3376f341a9068cad0d7bf503
spycat88 <spycat88@users.noreply.github.com>
2024-12-10 22:31:17 +0000

    Escape brackets to fix issue of single letter file breaking API call

--- es-app/src/ApiSystem.cpp
+++ es-app/src/ApiSystem.cpp
@@ -685,7 +685,7 @@ std::vector<std::string> ApiSystem::getAvailableAudioOutputDevices()
 
 std::vector<std::string> ApiSystem::getAvailableGovernors()
 {
-	return executeEnumerationScript("/usr/bin/sh -lc \"echo \\\"default\\\"; tr \\\" \\\" \\\"\\n\\\" < /sys/devices/system/cpu/cpufreq/policy0/scaling_available_governors\" | grep [a-z]");
+	return executeEnumerationScript("/usr/bin/sh -lc \"echo \\\"default\\\"; tr \\\" \\\" \\\"\\n\\\" < /sys/devices/system/cpu/cpufreq/policy0/scaling_available_governors\" | grep \[a-z\]");
 }
 
 std::vector<std::string> ApiSystem::getAvailableColors()
